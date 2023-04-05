850884ebc39c4dcf2b6e365c74a683bce3c52e35
mancinilucas <mancini.lucasg@gmail.com>
2023-04-05 13:43:17 -0300

    First Commit

---
+++ src/main/java/com/sblibrarycontrol/sbdesafiopadroesprojeto/SbDesafioPadroesProjetoApplication.java
@@ -0,0 +1,13 @@
+package com.sblibrarycontrol.sbdesafiopadroesprojeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbDesafioPadroesProjetoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbDesafioPadroesProjetoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sblibrarycontrol/sbdesafiopadroesprojeto/SbDesafioPadroesProjetoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sblibrarycontrol.sbdesafiopadroesprojeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbDesafioPadroesProjetoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
