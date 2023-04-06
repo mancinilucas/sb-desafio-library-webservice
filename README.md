4e7a20118e305897652c3d9ad8b36c698c12ba3d
mancinilucas <mancini.lucasg@gmail.com>
2023-04-06 11:04:49 -0300

    Operacoes de livros; Readme

---
+++ README.md
@@ -0,0 +1,31 @@
+
+# Desafio Padrões de Projeto - DIO/Orange Tech
+
+Através da parceria entre a plataforma de ensino Digital Innovation One e o Banco Inter foi realizado um bootcamp denominado Orange Tech com a proposta de desenvolver o conhecimento na linguagem de programação Java, framework Spring e o paradigma de Programação Orientada a Objetos.
+
+O desafio proposto possuía o objetivo de colocar em prática os conhecimentos adquiridos sobre padrões de projeto utilizando Java e Spring Framework, conteúdos expostos pelo instrutor Venilton Jr.
+
+## Stack utilizada
+
+
+**Tecnologias:**
+* Java 17
+* Spring Boot
+* JPA
+* Hibernate
+* Git
+* IDE Intellij
+
+
+
+
+## Autores
+
+- [Lucas Mancini](https://www.github.com/mancinilucas)
+
+- [LinkedIn](https://www.linkedin.com/in/lucasgmancini/)
+## Referência
+
+ - [DIO](https://www.dio.me/)
+
+
--- src/main/java/com/sblibrarycontrol/sbdesafiopadroesprojeto/services/BookService.java
+++ src/main/java/com/sblibrarycontrol/sbdesafiopadroesprojeto/services/BookService.java
@@ -1,8 +1,14 @@
 package com.sblibrarycontrol.sbdesafiopadroesprojeto.services;
 
 import com.sblibrarycontrol.sbdesafiopadroesprojeto.entities.Book;
+import com.sblibrarycontrol.sbdesafiopadroesprojeto.entities.Book;
 import com.sblibrarycontrol.sbdesafiopadroesprojeto.repositories.BookRepository;
+import com.sblibrarycontrol.sbdesafiopadroesprojeto.services.exceptions.DatabaseExceptions;
+import com.sblibrarycontrol.sbdesafiopadroesprojeto.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -20,6 +26,38 @@ public class BookService {
 
     public Book findById(Long id){
         Optional<Book> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
+    }
+
+    public Book insert(Book obj){
+        return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        try {
+            repository.deleteById(id);
+        }catch (EmptyResultDataAccessException e){
+            throw new ResourceNotFoundException(id);
+        }catch (DataIntegrityViolationException e){
+            throw new DatabaseExceptions(e.getMessage());
+        }
+
+    }
+
+    public Book update(Long id, Book obj){
+        try {
+            Book entity = repository.getReferenceById(id);
+            updateData(entity, obj);
+            return repository.save(entity);
+        }catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException(id);
+        }
+    }
+
+    private void updateData(Book entity, Book obj) {
+        entity.setTitle(obj.getTitle());
+        entity.setAuthor(obj.getAuthor());
+        entity.setReleaseYear(obj.getReleaseYear());
+
     }
 }
