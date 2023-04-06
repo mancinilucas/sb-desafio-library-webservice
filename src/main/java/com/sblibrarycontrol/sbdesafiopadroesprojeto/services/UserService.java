e20699e07aed78b6e6fceadb684a0181613745bd
mancinilucas <mancini.lucasg@gmail.com>
2023-04-06 10:15:03 -0300

    Exception handling - findById

--- src/main/java/com/sblibrarycontrol/sbdesafiopadroesprojeto/services/UserService.java
+++ src/main/java/com/sblibrarycontrol/sbdesafiopadroesprojeto/services/UserService.java
@@ -2,6 +2,7 @@ package com.sblibrarycontrol.sbdesafiopadroesprojeto.services;
 
 import com.sblibrarycontrol.sbdesafiopadroesprojeto.entities.User;
 import com.sblibrarycontrol.sbdesafiopadroesprojeto.repositories.UserRepository;
+import com.sblibrarycontrol.sbdesafiopadroesprojeto.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,10 +21,25 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = repository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     public User insert(User obj){
         return repository.save(obj);
+    }
+
+    public void delete(Long id){
+        repository.deleteById(id);
+    }
+
+    public User update(Long id, User obj){
+        User entity = repository.getReferenceById(id);
+        updateData(entity, obj);
+        return repository.save(entity);
+    }
+
+    private void updateData(User entity, User obj) {
+        entity.setName(obj.getName());
+        entity.setPhone(obj.getPhone());
     }
 }
